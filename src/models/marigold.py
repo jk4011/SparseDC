d9965e39df124f05c98dd14da3c9a1d786a11226
Jinhyeok <jinhyeok@unist.ac.kr>
2024-02-06 20:39:32 +0900

    marigold example

--- src/models/marigold.py
+++ src/models/marigold.py
@@ -1,4 +1,3 @@
-import csv
 import os
 from typing import Any, Dict, List
 
@@ -19,9 +18,39 @@ from src.utils.vis_utils import (
     batch_save_kitti,
     padding_kitti,
 )
+import sys
+sys.path.append("Marigold")
+
+import numpy as np
+from PIL import Image
+from torchvision import transforms
+from model import init_marigold, run_marigold
 
 
-class DepthLitModule(LightningModule):
+def compute_scale_and_shift(prediction, target, mask):
+    # system matrix: A = [[a_00, a_01], [a_10, a_11]]
+    a_00 = torch.sum(mask * prediction * prediction, (1, 2))
+    a_01 = torch.sum(mask * prediction, (1, 2))
+    a_11 = torch.sum(mask, (1, 2))
+
+    # right hand side: b = [b_0, b_1]
+    b_0 = torch.sum(mask * prediction * target, (1, 2))
+    b_1 = torch.sum(mask * target, (1, 2))
+
+    # solution: x = A^-1 . b = [[a_11, -a_01], [-a_10, a_00]] / (a_00 * a_11 - a_01 * a_10) . b
+    x_0 = torch.zeros_like(b_0)
+    x_1 = torch.zeros_like(b_1)
+
+    det = a_00 * a_11 - a_01 * a_01
+    valid = det.nonzero()
+
+    x_0[valid] = (a_11[valid] * b_0[valid] - a_01[valid] * b_1[valid]) / det[valid]
+    x_1[valid] = (-a_01[valid] * b_0[valid] + a_00[valid] * b_1[valid]) / det[valid]
+
+    return x_0, x_1
+
+
+class MarigoldModule(LightningModule):
     """Example of LightningModule for MNIST classification.
 
     A LightningModule organizes your PyTorch code into 6 sections:
@@ -38,24 +67,16 @@ class DepthLitModule(LightningModule):
 
     def __init__(
         self,
-        net: torch.nn.Module,
-        optimizer: torch.optim.Optimizer,
-        scheduler: torch.optim.lr_scheduler,
         metric,
         monitor,
         save_dir,
         base_lr,
         dataset,
         is_warmup=False,
+        
     ):
         super().__init__()
-
-        # this line allows to access init params with 'self.hparams' attribute
-        # also ensures init params will be stored in ckpt
-        self.save_hyperparameters(logger=False)
-
-        self.net = net
-        self.module_name = self.net.__class__.__name__
+        self.model = init_marigold()
 
         self.is_warmup = is_warmup
 
@@ -70,87 +91,37 @@ class DepthLitModule(LightningModule):
             self.figure_dir = os.path.join(self.save_dir, "val_results")
             os.makedirs(self.figure_dir, exist_ok=True)
 
+    def load_state_dict(self, path):
+        pass
+
     def forward(self, batch):
-        return self.net(batch)
+        # TODO: loop 
+        assert len(batch['rgb']) == 1
+        
+        image = batch['rgb'][0].cpu()
+        gt_depth = batch['gt'][0]
+        mask = batch['dep'][0] != 0
+        
+        depth = run_marigold(self.model, image)
+        scale, shift = compute_scale_and_shift(depth, gt_depth, mask)
+        depth = scale.view(-1, 1, 1) * depth + shift.view(-1, 1, 1)
 
-    def on_train_start(self):
-        # by default lightning executes validation step sanity checks before training starts,
-        # so we need to make sure val_acc_best doesn't store accuracy from these checks
-        if self.is_warmup:
-            self.warmup_iters = len(self.trainer.train_dataloader)
-        if is_master():
-            with open(self.val_csv, "w") as csvfile:
-                writer = csv.DictWriter(csvfile, fieldnames=self.fieldnames)
-                writer.writeheader()
-        self.avg_loss = 0
-        self.count = 0
+        return depth
 
-        self.best_result.reset()
+    # def training_step(self, batch: Any, batch_idx: int):
+    #     return 0
 
-    def training_step(self, batch: Any, batch_idx: int):
-        _, loss, loss_val = self.forward(batch)
-        self.avg_loss = (self.avg_loss * self.count + loss.item()) / (self.count + 1)
-        self.count += 1
-        self.log("avg_loss", self.avg_loss, on_epoch=False, prog_bar=True)
-        self.log_dict(loss_val, on_epoch=False, prog_bar=False)
-        return loss
-
-    def training_epoch_end(self, outputs: List[Any]):
-        # `outputs` is a list of dicts returned from `training_step()`
-
-        # Warning: when overriding `training_epoch_end()`, lightning accumulates outputs from all batches of the epoch
-        # this may not be an issue when training on mnist
-        # but on larger datasets/models it's easy to run into out-of-memory errors
-
-        # consider detaching tensors before returning them from `training_step()`
-        # or using `on_train_epoch_end()` instead which doesn't accumulate outputs
-        self.metric.reset()
-
-    def on_validation_epoch_start(self):
-        self.output_csv = os.path.join(self.save_dir, "output.csv")
-        with open(self.output_csv, "w") as csvfile:
-            writer = csv.DictWriter(csvfile, fieldnames=self.fieldnames)
-            writer.writeheader()
 
     def validation_step(self, batch: Any, batch_idx: int):
         gt = batch["gt"]
-        pred, _, loss_val = self.forward(batch)
+        pred = self.forward(batch)
         self.metric.evaluate(pred, gt)
         rank, word_size = get_dist_info()
         result = {}
         result["epoch"] = batch_idx * word_size + rank
         for k in self.metric.metrics.keys():
             result[k] = f"{self.metric.metrics[k].item():.5f}"
-        with open(self.output_csv, "a") as csvfile:
-            writer = csv.DictWriter(csvfile, fieldnames=self.fieldnames)
-            writer.writerow(result)
-        self.log_dict(
-            loss_val, on_epoch=False, sync_dist=True, prog_bar=False, on_step=True
-        )
-        if batch_idx == 0:
-            if is_master():
-                out_path = os.path.join(
-                    self.figure_dir, f"epoch_{self.current_epoch}.png"
-                )
-                if self.hparams.dataset in ["nyu", "sunrgbd"]:
-                    batch_save(
-                        batch["rgb"],
-                        batch["dep"],
-                        pred,
-                        gt,
-                        (pred - gt).abs(),
-                        out_path,
-                        self.hparams.dataset,
-                    )
-                elif self.hparams.dataset == "kitti":
-                    batch_save_kitti(
-                        batch["rgb"],
-                        batch["dep"],
-                        pred,
-                        gt,
-                        (pred - gt).abs(),
-                        out_path,
-                    )
+        
 
     def validation_epoch_end(self, outputs: List[Any]):
         avg_metric = self.metric.average()
@@ -163,28 +134,16 @@ class DepthLitModule(LightningModule):
         )
         self.metric.reset()
         avg_metric["epoch"] = self.current_epoch
-        self.save_val_result(avg_metric)
         try:
             self.lr = self.optimizers().param_groups[0]["lr"]
         except:
             pass
 
-    def on_test_start(self):
-        super().on_test_start()
-        self.test_out_dir = os.path.join(self.save_dir, "test")
-        os.makedirs(self.test_out_dir, exist_ok=True)
-        self.test_csv = os.path.join(self.test_out_dir, f"{self.module_name}.csv")
-        self.test_fieldnames = ["filename"] + list(self.metric.metrics.keys())
-        with open(self.test_csv, "w") as f:
-            writer = csv.DictWriter(f, fieldnames=self.test_fieldnames)
-            writer.writeheader()
-
     def test_step(self, batch: Any, batch_idx: int):
         rank, word_size = get_dist_info()
-        pred, _, loss_val = self.forward(batch)
-        self.log_dict(
-            loss_val, on_epoch=False, sync_dist=True, prog_bar=False, on_step=True
-        )
+        pred = self.forward(batch)
+        import jhutil; jhutil.jhprint(1111, self.hparams)
+
         if self.hparams.dataset in ["nyu", "sunrgbd"]:
             gt = batch["gt"]
             self.metric.evaluate(pred, gt)
@@ -192,9 +151,6 @@ class DepthLitModule(LightningModule):
             result["filename"] = batch_idx * word_size + rank
             for k in self.metric.metrics.keys():
                 result[k] = f"{self.metric.metrics[k].item():.5f}"
-            with open(self.test_csv, "a") as csvfile:
-                writer = csv.DictWriter(csvfile, fieldnames=self.test_fieldnames)
-                writer.writerow(result)
         str_i = str(batch_idx * word_size + rank)
         path_i = str_i.zfill(10) + ".png"
         path = os.path.join(self.test_out_dir, path_i)
@@ -225,7 +181,6 @@ class DepthLitModule(LightningModule):
                 )
             self.metric.reset()
             avg_metric["epoch"] = "Test"
-            self.save_val_result(avg_metric)
 
     def configure_optimizers(self):
         """Choose what optimizers and learning-rate schedulers to use in your optimization.
@@ -273,15 +228,3 @@ class DepthLitModule(LightningModule):
                 for pg in optimizer.param_groups:
                     pg["lr"] = lr_warm_up
 
-    def save_val_result(self, result):
-        if is_master():
-            with open(self.val_csv, "a") as csvfile:
-                writer = csv.DictWriter(csvfile, fieldnames=self.fieldnames)
-                for key in result.keys():
-                    if key != "epoch":
-                        result[key] = "{:.5f}".format(result[key].data)
-                writer.writerow(result)
-
-
-if __name__ == "__main__":
-    _ = DepthLitModule(None, None, None)
